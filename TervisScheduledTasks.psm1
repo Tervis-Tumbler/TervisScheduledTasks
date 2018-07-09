9d691e93fb41df57b0b1f936f2d890f1bc0a9631
ChrisMagnuson <hourglasssand@gmail.com>
2018-07-09 16:40:36 -0400

    Refactor to store repetition intervals in separate definition file

--- TervisScheduledTasks.psm1
+++ TervisScheduledTasks.psm1
@@ -1,4 +1,5 @@
-﻿#requires -module TervisApplication
+﻿$ModulePath = (Get-Module -ListAvailable TervisScheduledTasks).ModuleBase
+. $ModulePath\Definition.ps1
 
 function Install-TervisScheduledTask {
     param (
@@ -86,77 +87,6 @@ function Get-RepetitionInterval {
     )
     $RepetitionIntervals | 
     where {-Not $Name -or $_.Name -EQ $Name}
-}
-
-$RepetitionIntervals = [PSCustomObject][Ordered]@{
-    Name = "EveryMinuteOfEveryDay"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 12am)
-    TaskTriggersRepetitionDuration = "P1D"
-    TaskTriggersRepetitionInterval = "PT1M"
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayEver5Minutes"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 12am)
-    TaskTriggersRepetitionDuration = "P1D"
-    TaskTriggersRepetitionInterval = "PT5M"
-},
-[PSCustomObject][Ordered]@{
-    Name = "OnceAWeekMondayMorning"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday -At 8am)
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayAt3am"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 3am)
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayAt6am"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 6am)
-},
-[PSCustomObject][Ordered]@{
-    Name = "OnceAWeekTuesdayMorning"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Weekly -DaysOfWeek Tuesday -At 8am)
-},
-[PSCustomObject][Ordered]@{
-    Name = "EverWorkdayDuringTheDayEvery15Minutes"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At 7am)
-    TaskTriggersRepetitionDuration = "PT12H"
-    TaskTriggersRepetitionInterval = "PT15M"
-},
-[PSCustomObject][Ordered]@{
-    Name = "EverWorkdayOnceAtTheStartOfTheDay"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At 7am)
-},
-[PSCustomObject][Ordered]@{
-    Name = "EverWorkdayAt1PM"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At 1pm)
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayEvery15Minutes"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 12am)
-    TaskTriggersRepetitionDuration = "P1D"
-    TaskTriggersRepetitionInterval = "PT15M"
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayAt2am"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 2am)
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayAt5amEvery3HoursFor18Hours"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 5am)
-    TaskTriggersRepetitionDuration = "PT18H"
-    TaskTriggersRepetitionInterval = "PT3H"
-},
-[PSCustomObject][Ordered]@{
-    Name = "EveryDayAt7am3pm11pm"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 7am),
-    $(New-ScheduledTaskTrigger -Daily -At 3pm),
-    $(New-ScheduledTaskTrigger -Daily -At 11pm)
-},
-[PSCustomObject][Ordered]@{
-    Name = "Every12HoursEveryDay"
-    ScheduledTaskTrigger = $(New-ScheduledTaskTrigger -Daily -At 12am)
-    TaskTriggersRepetitionDuration = "P1D"
-    TaskTriggersRepetitionInterval = "PT12H"
 }
 
 function Invoke-ScheduledTasksProvision {
